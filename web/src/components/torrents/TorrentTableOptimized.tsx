253fef7c6baff1c7db4b37acad3191dfe197d7f4
soup <s0up4200@pm.me>
2025-10-11 01:16:33 +0200

    add comment

--- web/src/components/torrents/TorrentTableOptimized.tsx
+++ web/src/components/torrents/TorrentTableOptimized.tsx
@@ -1309,6 +1309,7 @@ export const TorrentTableOptimized = memo(function TorrentTableOptimized({ insta
 
       {/* Table container */}
       <div className="flex flex-col flex-1 min-h-0 mt-2 sm:mt-0 overflow-hidden">
+        {/* Virtual scroll container with paint containment optimization for improved rendering performance */}
         <div
           className="relative flex-1 overflow-auto scrollbar-thin select-none will-change-transform contain-paint"
           ref={parentRef}
