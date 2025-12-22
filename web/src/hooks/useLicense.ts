948e2986c084f9d8913ebd1c89f9288e5d8cda9a
thesecretlifeofabunny <212422141+thesecretlifeofabunny@users.noreply.github.com>
2025-12-21 19:51:45 -0800

    set staleTime to be same as refetchInterval.

--- web/src/hooks/useLicense.ts
+++ web/src/hooks/useLicense.ts
@@ -8,7 +8,6 @@ import { getLicenseErrorMessage } from "@/lib/license-errors.ts"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { toast } from "sonner"
 
-const thirtySecondsInMilliseconds: number = 30 * 1000;
 const thirtyMinutesInMilliseconds: number = 30 * 1000 * 60;
 const oneHourInMilliseconds: number = 60 * 1000 * 60;
 
@@ -17,7 +16,7 @@ export const usePremiumAccess = () => {
   return useQuery({
     queryKey: ["licenses"],
     queryFn: () => api.getLicensedThemes(),
-    staleTime: thirtySecondsInMilliseconds,
+    staleTime: oneHourInMilliseconds,
     refetchInterval: oneHourInMilliseconds,
     refetchOnWindowFocus: false,
   })
