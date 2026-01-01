62a7ad5e11434e99badff333943d3df45bca2c91
soup <s0up4200@pm.me>
2026-01-01 18:39:10 +0100

    feat(docs): use qui favicon

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 const config: Config = {
   title: "qui",
   tagline: "Modern web interface for qBittorrent",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon.png",
 
   url: "https://getqui.com",
   baseUrl: "/",
