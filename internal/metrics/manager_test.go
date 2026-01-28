e0045c5234e8a0e26d83ea70283404710c261c70
Oscar Ekeroth <oekeroth@baffinbaynetworks.com>
2026-01-28 22:32:57 +0100

    Update metrics tests to reflect the new number of metrics

--- internal/metrics/manager_test.go
+++ internal/metrics/manager_test.go
@@ -98,7 +98,7 @@ func TestTorrentCollector_Describe(t *testing.T) {
 	}
 
 	// Should have all expected metrics descriptors
-	assert.Len(t, descs, 11, "Should have 11 metric descriptors")
+	assert.Len(t, descs, 7, "Should have 7 metric descriptors")
 }
 
 func TestTorrentCollector_CollectWithNilDependencies(t *testing.T) {
