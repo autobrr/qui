c4f581dbaf5d0fb54771af986df847e09480bcda
soup <s0up4200@pm.me>
2026-02-06 14:53:30 +0100

    test(automations): cover resume dry-run activity

--- internal/services/automations/service_test.go
+++ internal/services/automations/service_test.go
@@ -1340,6 +1340,49 @@ func TestRecordDryRunActivities_Deletes(t *testing.T) {
 	assert.Equal(t, models.ActivityOutcomeDryRun, mockDB.activities[0].Outcome)
 }
 
+func TestRecordDryRunActivities_Resumes(t *testing.T) {
+	mockDB := &mockQuerier{
+		activities: make([]*models.AutomationActivity, 0),
+	}
+	activityStore := models.NewAutomationActivityStore(mockDB)
+
+	sm := qbittorrent.NewSyncManager(nil, nil)
+	s := &Service{
+		activityStore: activityStore,
+		activityRuns:  newActivityRunStore(24*time.Hour, 10),
+		syncManager:   sm,
+	}
+
+	torrent := qbt.Torrent{
+		Hash:    "abc123",
+		Name:    "Test Torrent",
+		Tracker: "https://tracker.example.com/announce",
+	}
+
+	s.recordDryRunActivities(
+		context.Background(),
+		1,
+		nil,
+		nil,
+		nil,
+		nil,
+		[]string{"abc123", "abc123"},
+		nil,
+		nil,
+		nil,
+		nil,
+		nil,
+		map[string]qbt.Torrent{"abc123": torrent},
+		[]qbt.Torrent{torrent},
+		map[string]*torrentDesiredState{},
+	)
+
+	require.Len(t, mockDB.activities, 1)
+	assert.Empty(t, mockDB.activities[0].Hash)
+	assert.Equal(t, models.ActivityActionResumed, mockDB.activities[0].Action)
+	assert.Equal(t, models.ActivityOutcomeDryRun, mockDB.activities[0].Outcome)
+}
+
 // mockQuerier implements dbinterface.Querier for testing activity logging
 type mockQuerier struct {
 	activities []*models.AutomationActivity
