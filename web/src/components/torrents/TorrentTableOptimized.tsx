3c5cf3bb4f4faeb3b85e433198dbd2ad539449c3
martylukyy <35452459+martylukyy@users.noreply.github.com>
2026-05-11 11:43:23 +0200

    feat(torrents): implement auto-fit width for columns on double-click (#1867)

--- web/src/components/torrents/TorrentTableOptimized.tsx
+++ web/src/components/torrents/TorrentTableOptimized.tsx
@@ -2980,6 +2980,7 @@ export const TorrentTableOptimized = memo(function TorrentTableOptimized({
                           return (
                             <div
                               key={cell.id}
+                              data-torrent-column-measure={cell.column.id}
                               style={{
                                 width: cell.column.getSize(),
                                 flexShrink: 0,
