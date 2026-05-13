b5afa20c76d52017217ccccaca1a5d8ad7d57660
soup <s0up4200@pm.me>
2026-05-13 20:17:13 +0200

    fix(qbittorrent): handle new tracker statuses

--- internal/qbittorrent/sync_manager.go
+++ internal/qbittorrent/sync_manager.go
@@ -2596,7 +2596,7 @@ func (sm *SyncManager) torrentIsUnregistered(torrent *qbt.Torrent) bool {
 			continue
 		case qbt.TrackerStatusOK:
 			hasWorking = true
-		case qbt.TrackerStatusUpdating, qbt.TrackerStatusNotWorking:
+		case qbt.TrackerStatusUpdating, qbt.TrackerStatusNotWorking, qbt.TrackerStatusTrackerError:
 			if trackerMessageMatches(tracker.Message, defaultUnregisteredStatuses) {
 				hasUnregistered = true
 			}
@@ -2620,7 +2620,9 @@ func (sm *SyncManager) torrentTrackerIsDown(torrent *qbt.Torrent) bool {
 			continue
 		case qbt.TrackerStatusOK, qbt.TrackerStatusUpdating:
 			hasWorking = true
-		case qbt.TrackerStatusNotWorking:
+		case qbt.TrackerStatusUnreachable:
+			hasDown = true
+		case qbt.TrackerStatusNotWorking, qbt.TrackerStatusTrackerError:
 			if TrackerMessageMatchesDown(tracker.Message) {
 				hasDown = true
 			}
